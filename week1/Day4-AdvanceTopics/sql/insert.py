909aab807597ae6e469d64b79b9d6a0cb26bc4e3
farehailyas <fareha.ilyas@datumlabs.io>
2026-06-30 14:42:36 +0500

    Delete week1 directory

--- week1/Day4-AdvanceTopics/sql/insert.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con = sqlite3.Connection("mydb.db")
-cr = con.cursor()
-
-# insert into db 
-insert = """INSERT INTO emp VALUES(4,"FAREHA" ,"TRAINEE" , "1-06-2026")"""
-cr.execute(insert)
-con.commit()
